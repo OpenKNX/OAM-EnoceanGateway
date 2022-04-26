a9ea40406331060225a145a72e96c3f00ad2d987
Waldemar Porscha <waldemar.porscha@sap.com>
2022-04-27 00:36:05 +0200

    Release preparation - Easier integration with logic - No logic hardware settings anymore - updated build script

--- scripts/Build-knxprod.ps1
+++ scripts/Build-knxprod.ps1
@@ -9,9 +9,14 @@ if ($toolsExist) {
 if (!$toolsExist) {
     Write-Host "
         Fuer das Setup fehlen die notwendigen OpenKNX-Tools oder sie sind veraltet..
-        Bitte das neuste Paket herunterladen 
+        Bitte das neuste Paket herunterladen
+
             https://github.com/OpenKNX/OpenKNXproducer/releases
-        entpacken und das Readme befolgen.
+        
+        entpacken und das Readme befolgen. Weitere Informationen hierzu gibt es im OpenKNX-Wiki
+
+            https://github.com/OpenKNX/OpenKNX/wiki/Installation-of-OpenKNX-tools
+
         Danach bitte dieses Script erneut starten.
 
         ACHTUNG: Heutige Browser warnen vor dem Inhalt des OpenKNX-Tools Pakets, 
