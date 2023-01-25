2d742542e5def5a36ae1d7f7eec7e68552c42eee
DIY-Smarthome-Admin <boarder_ulm@web.de>
2023-01-25 13:18:09 +0100

    Bugfix: A5-20-xx  funktion "round"

--- src/EnoceanHandle.h
+++ src/EnoceanHandle.h
@@ -12,7 +12,7 @@
 #include "Helper.h"
 #include "KnxHelper.h"
 
-#define round(x)    ((unsigned) ((x) + .5))
+#define round(x)    ((uint8_t) ((x) + .5))
 
 // ################################################
 // ### DEBUG CONFIGURATION
@@ -626,7 +626,7 @@ public:
           else
           {
             union1.val_A5_20_01[0] = (uint8_t)iKo.value(getDPT(VAL_DPT_5)); // Set Point Pos
-            round(union1.val_A5_20_01[0] = union1.val_A5_20_01[0] / 2.55);
+            union1.val_A5_20_01[0] = round(union1.val_A5_20_01[0] / 2.55);
 #ifdef KDEBUG
             SERIAL_PORT.print(F("SET Pos to: "));
             SERIAL_PORT.print(union1.val_A5_20_01[0]);
@@ -724,7 +724,7 @@ public:
         {
         case KO_0: //  SET Pos
           union1.val_A5_20_04[0] = (uint8_t)iKo.value(getDPT(VAL_DPT_5));
-          round(union1.val_A5_20_04[0] = union1.val_A5_20_04[0] / 2.55);
+          union1.val_A5_20_04[0] = round(union1.val_A5_20_04[0] / 2.55);
 #ifdef KDEBUG
           SERIAL_PORT.print(F("SET Pos to: "));
           SERIAL_PORT.print(union1.val_A5_20_04[0]);
@@ -847,7 +847,7 @@ public:
           else
           {
             union1.val_A5_20_06[0] = (uint8_t)iKo.value(getDPT(VAL_DPT_5)); // Set Point Pos
-            round(union1.val_A5_20_06[0] = union1.val_A5_20_06[0] / 2.55);
+            union1.val_A5_20_06[0] = round(union1.val_A5_20_06[0] / 2.55);
 #ifdef KDEBUG
             SERIAL_PORT.print(F("SET Pos to: "));
             SERIAL_PORT.print(union1.val_A5_20_06[0]);
